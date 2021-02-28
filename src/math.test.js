00ea528a92209bdaa665f0b8b4e2931e20fe0741
btchoum <bertrand.tchoumkeu@gmail.com>
2021-02-27 18:50:02 -0600

    add tests for multiply

--- src/math.test.js
+++ src/math.test.js
@@ -1,5 +1,9 @@
-const { add } = require("./math");
+const { add, multiply } = require("./math");
 
-test("it adds two numbers", function () {
+test("it adds two numbers", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test("it can multiply", () => {
+  expect(multiply(2, 3)).toBe(7);
 });
